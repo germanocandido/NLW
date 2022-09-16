f8b74ee1bef23a6be10ab110afdab3d3d6849c52
germanocandido <germanocandido@msn.com>
2022-09-16 20:15:50 -0300

    feat: Adicionar legenda aos jogos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,27 +9,47 @@ function App() {
       <h1 className='text-6xl text-white font-black mt-20'>Seu  <span className='bg-nlw-gradient text-transparent bg-clip-text'>DUO</span> está aqui</h1>
     
       <div className='grid grid-cols-6 gap-6 mt-16'>
-       <a href="" className='relative'>
+       <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/public/assets/games/game-1.png" />
-        <div className=' w-full pt-16 pb-4 px-4 bg-game-gradient'>
+        <div className=' w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
           <strong className='font-bold text-white'>League of Legends</strong>
           <span className='text-zinc-300 text-sm block mt-1'>anúncios</span>
         </div>
        </a>
-       <a href="" className='relative'>
+       <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/public/assets/games/game-2.png" />
+        <div className=' w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white'>Dota 2</strong>
+          <span className='text-zinc-300 text-sm block mt-1'>anúncios</span>
+        </div>
        </a>
-       <a href="" className='relative'>
+       <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/public/assets/games/game-3.png" />
+        <div className=' w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white'>CS:GO</strong>
+          <span className='text-zinc-300 text-sm block mt-1'>anúncios</span>
+        </div>
        </a>
-       <a href="" className='relative'>
+       <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/public/assets/games/game-4.png" />
+        <div className=' w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white'>Apex Legend</strong>
+          <span className='text-zinc-300 text-sm block mt-1'>anúncios</span>
+        </div>
        </a>
-       <a href="" className='relative'>
+       <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/public/assets/games/game-5.png" />
+        <div className=' w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white'>Fortnite</strong>
+          <span className='text-zinc-300 text-sm block mt-1'>anúncios</span>
+        </div>
        </a>
-       <a href="" className='relative'>
+       <a href="" className='relative rounded-lg overflow-hidden'>
         <img src="/public/assets/games/game-6.png" />
+        <div className=' w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white'>WOW</strong>
+          <span className='text-zinc-300 text-sm block mt-1'>anúncios</span>
+        </div>
        </a>
 
 
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       backgroundImage: {
         galaxy: "url('/public/assets/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(50.86deg, #9572FC 35.08%, #43E7AD 50.94%, #E1D55D 44.57%)',
-        'game-gradient':'background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+        'game-gradient':'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
         
       }
     },
