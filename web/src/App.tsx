e5d5c6ce40cc5ca8166469141a9f6266472852fd
germanocandido <germanocandido@msn.com>
2022-09-16 20:55:33 -0300

    feat: Menu inferior + botão

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/main.css';
 import logoImg from "/public/assets/logo-nlw-esports.png"
+import {MagnifyingGlassPlus} from 'phosphor-react'
 
 
 function App() {
@@ -51,10 +52,19 @@ function App() {
           <span className='text-zinc-300 text-sm block mt-1'>anúncios</span>
         </div>
        </a>
-
-
       </div>
-
+      <div className='pt-1 self-stretch bg-nlw-gradient rounded-lg overflow-hidden mt-8'>
+      <div  className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+              <strong className='text-2xl text-white font-black block'>Não encontrou o seu duo?</strong>
+              <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players</span>
+          </div>
+              <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+                   <MagnifyingGlassPlus size={24}/>
+                    Publicar Anuncio
+              </button>
+        </div>  
+      </div>
     </div>
   )
 }
