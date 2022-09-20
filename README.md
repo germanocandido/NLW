b563946dd084cbace388d56e784686c5aeb96b87
germanocandido <germanocandido@msn.com>
2022-09-20 17:21:05 -0300

    feat: Adicionando as rotas

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # NLW
 Projeto desenvolvido da Maratona NLW
+
+# Back-end
+
+## Entidades
+
+### Game
+
+id
+title
+BannerUrl
+
+### Ad -
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+
+##4 Casos de usos
+- Listagem de games
+- Criação de anúncio
+- Listagem de anúncio por  Game
+- Buscar Discord pelo Id do anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,26 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (req, res) => {
+        return res.json([])
+    });
+
+app.post('/ads', (req, res) => {
+        return res.status(201).json([])
+    });
+
+
+app.get('/games/:id/ads', (request, response) => {
     return response.json([
         {id: 1, name: 'Anuncio 1'},
         {id: 2, name: 'Anuncio 2'},
         {id: 3, name: 'Anuncio 3'},
         {name: 'sfdsdfsd'}
     ]);
+})
+
+app.get('/games/:id/discord', (request, response) => {
+    return response.json([]);
 })
 
 app.listen(3333)
