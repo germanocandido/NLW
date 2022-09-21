862987e3bc3c87d4e8b1320bdbf71c144be25ad3
germanocandido <germanocandido@msn.com>
2022-09-20 21:02:23 -0300

    fest: adicionando a const do ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,15 @@ const app = express();
 const prisma = new PrismaClient()
 
 app.get('/games', async(req, res) => {
-    const games = await prisma.game.findMany()
+    const games = await prisma.game.findMany({
+        include:{
+          _count:{
+            select: {
+                Ads: true,
+            }
+          }
+        }
+    })
 
 
         return res.json(games)
@@ -16,13 +24,36 @@ app.post('/ads', (req, res) => {
     });
 
 
-app.get('/games/:id/ads', (request, response) => {
-    return response.json([
-        {id: 1, name: 'Anuncio 1'},
-        {id: 2, name: 'Anuncio 2'},
-        {id: 3, name: 'Anuncio 3'},
-        {name: 'sfdsdfsd'}
-    ]);
+app.get('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+            
+        },
+        where:{
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    })
+    
+
+
+
+    return response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }));
 })
 
 app.get('/games/:id/discord', (request, response) => {
